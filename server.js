520dbdd18f25aece2846e7142f494fe3ead2b9ec
Fahmi Tresna Yudha <fahmi24003@mail.unpad.ac.id>
2026-06-21 22:55:59 +0700

    Update server to use environment variable for PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const db = require('./db');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -134,7 +134,7 @@ app.delete('/api/bookings/:id', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/Care+.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(PORT, () => {
